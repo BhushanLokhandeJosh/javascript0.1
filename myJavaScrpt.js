8fab029d806d3ddb1f128bfd98d587e51a55d982
BhushanLokhandeJosh <bhushan.lokhande@joshsoftware.com>
2022-11-22 21:58:33 +0530

    All Updates done

--- myJavaScrpt.js
+++ myJavaScrpt.js
@@ -2,9 +2,9 @@
 
 function BhushanFunction() {
 
-    var bhushan = new Date();
-    window.alert("In my function at "+bhushan);
-    document.getElementById("demo").setAttribute("style","color:Red");
+    var timeAtWhichFunctionCalled = new Date();
+    window.alert("In my function at "+timeAtWhichFunctionCalled);
+    document.getElementById("myid").setAttribute("style","color:Red");
     document.getElementById("myid").innerHTML = "I Recently Joined as Software Engineer in Josh Software PVT. LTD.";
 
 }
