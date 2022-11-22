0b4c4c4be56d9886e8be392a36db33734df045f5
BhushanLokhandeJosh <bhushan.lokhande@joshsoftware.com>
2022-11-22 17:33:08 +0530

    All assignments added

---
+++ myJavaScrpt.js
@@ -0,0 +1,10 @@
+
+
+function BhushanFunction() {
+
+    var bhushan = new Date();
+    window.alert("In my function at "+bhushan);
+    document.getElementById("demo").setAttribute("style","color:Red");
+    document.getElementById("myid").innerHTML = "I Recently Joined as Software Engineer in Josh Software PVT. LTD.";
+
+}
